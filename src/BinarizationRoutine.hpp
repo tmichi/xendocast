978a8173d375f2697072f5350e3fb995fea62032
Takashi Michikawa <tmichi@me.com>
2016-05-10 18:02:51 +0900

    snapshot

--- src/BinarizationRoutine.hpp
+++ src/BinarizationRoutine.hpp
@@ -1,11 +1,10 @@
 #ifndef BINARIZATION_ROUTINE_HPP
 #define BINARIZATION_ROUTINE_HPP 1
-
+#include <iostream>
+#include <memory>
 #include <mi/Routine.hpp>
 #include <mi/Range.hpp>
 #include <mi/VolumeDataUtility.hpp>
-#include <iostream>
-#include <memory>
 
 template <typename T>
 class BinarizationRoutine: public mi::Routine {
@@ -16,7 +15,7 @@ private:
 public:
 	BinarizationRoutine ( const mi::VolumeData<T>& ctData, mi::VolumeData<char>& binaryData ) : 
 		mi::Routine ("binarization"), _ctData(ctData), _binaryData(binaryData), _threshold(T()) {
-		// check validity here
+		
 		return;
 	}
 	
@@ -30,7 +29,7 @@ public:
 		mi::VolumeDataUtility::binarize(this->_ctData, this->_binaryData, this->_isovalue);
 		// Extract largest component.
 		mi::VolumeDataUtility::extract_nth_component( tmpData, binaryData, 1 );
-		// Invert 
+
 		mi::VolumeDataUtility::invert(binaryData);
 		return true;
 	}
